f8c2319f8ac448d095c815b1482afee4846f2b9e
couchette <luwei000119@qq.com>
2024-02-27 00:49:29 +0800

    add docker scripts

---
+++ build_and_deploy_docker.bat
@@ -0,0 +1,8 @@
+@ echo off
+npm run build:renderer
+docker image ls
+docker ps -a
+docker stop electron-react-app && docker rm electron-react-app
+docker rmi electron-react-app
+docker build -t electron-react-app -f dockerfile_deploy .
+docker run -d --name electron-react-app -p 3000:3000 electron-react-app
